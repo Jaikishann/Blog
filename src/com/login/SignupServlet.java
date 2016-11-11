8da7ffea322c3ec3ae00f036355cffce84aee161
Jaikishann <kizhanz@gmail.com>
2016-11-11 18:45:57 +0530

    added validation but its not working

--- src/com/login/SignupServlet.java
+++ src/com/login/SignupServlet.java
@@ -7,31 +7,47 @@ import javax.servlet.http.*;
 
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+import com.google.appengine.api.datastore.KeyFactory;
+//import com.google.appengine.repackaged.com.google.api.client.util.Key;
 
 @SuppressWarnings("serial")
 public class SignupServlet extends HttpServlet {
 	public void doPost(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
-		com.google.appengine.api.datastore.DatastoreService ds=DatastoreServiceFactory.getDatastoreService();
-		PrintWriter out=resp.getWriter();
+		com.google.appengine.api.datastore.DatastoreService ds = DatastoreServiceFactory
+				.getDatastoreService();
+		PrintWriter out = resp.getWriter();
 		resp.setContentType("text/html");
-		String emailId=req.getParameter("emailId");
-		String userName=req.getParameter("username");
-		String password=req.getParameter("password");
-		if(emailId!=null&&userName!=null&&password!=null){
-			Entity user=new Entity("UserDetails");
-			user.setProperty("emailId", emailId);
-			user.setProperty("userName", userName);
-			user.setProperty("password", password);
-			ds.put(user);
-			out.print("<p>Your signup was successfull</p>");
-			out.print("<a href=\"ProfileServlet\"> Profile</a>");
-			out.print("<a href=\"LogoutServlet\"> Logout</a>");
+		String emailId = req.getParameter("emailId");
+		String userName = req.getParameter("username");
+		String password = req.getParameter("password");
+		//if (Validation.nullCheck(emailId, userName, password)) {
+			Entity user = new Entity("UserDetails", userName);
+
+			com.google.appengine.api.datastore.Key key = KeyFactory.createKey("userDetails", userName);
 			
-			
+			try {
+				Entity ent = ds.get(key);
+				
+				if (!(ent.getProperty("userName").equals(userName))) {
+					user.setProperty("emailId", emailId);
+					user.setProperty("userName", userName);
+					user.setProperty("password", password);
+					ds.put(user);
+					
+					out.print("<p>Your signup was successfull</p>");
+					out.print("<a href=\"ProfileServlet\"> Profile</a>");
+					out.print("<a href=\"LogoutServlet\"> Logout</a>");
+				} else {
+					out.print("<p>Please Enter Valid Details</p>");
+				}
+			} catch (EntityNotFoundException e) {
+				// TODO Auto-generated catch block
+				out.print("excp");
 			}
-		else{
-			out.print("<p>Please Enter Valid Details</p>");
+
 		}
+
 	}
-}
+//}
